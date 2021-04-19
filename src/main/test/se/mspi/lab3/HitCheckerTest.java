079b6c2c46e10acef13d7efd1a9aa2b4000e955d
Vsev0l0d <vsevolod628@gmail.com>
2021-04-19 18:21:11 +0300

    начал тесты

--- src/main/test/se/mspi/lab3/HitCheckerTest.java
+++ src/main/test/se/mspi/lab3/HitCheckerTest.java
@@ -1,4 +1,18 @@
 package se.mspi.lab3;
 
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+
 public class HitCheckerTest {
+    private HitChecker hitChecker;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void whenNegativeRadius(){
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Радиус не может быть меньше 0");
+        hitChecker.shoot(69, 42, -1);
+    }
 }
